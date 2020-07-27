60e7e466d236a8528fb4e2edbcab5a4f945238f3
Daylon <daylonjanis@gmail.com>
2020-07-27 17:50:33 -0400

    Add project files.

---
+++ MCCBounceEnable/src/UIForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MCCBounceEnable
+{
+    partial class UIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UIForm));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(97, 46);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Bounce Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MCCBounceEnable";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please ensure that you are in a H2C\r\ncustom game before enabling. It is\r\nrecommen" +
+    "ded to disable bounces\r\nbefore switching maps.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Daylon#0001 on Discord for questions";
+            // 
+            // UIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 189);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UIForm";
+            this.Text = "MCCBounceEnable";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MCCBounceEnable/src/UIForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Process.NET;
+using Process.NET.Memory;
+using Process.NET.Patterns;
+
+namespace MCCBounceEnable
+{
+    public partial class UIForm : Form
+    {
+        public readonly IMemoryPattern test = new DwordPattern("48 8B 0D ?? ?? ?? ?? F3 0F 10 49 10");
+        public readonly IMemoryPattern tickRate60 = new DwordPattern("3C 00 89 88 88 3C");
+        public readonly IMemoryPattern tickRate30 = new DwordPattern("1E 00 89 88 08 3D");
+
+        byte[] value30 = { 0x1E, 0x00, 0x89, 0x88, 0x08, 0x3D };
+        byte[] value60 = { 0x3C, 0x00, 0x89, 0x88, 0x88, 0x3C };
+
+        public UIForm()
+        {
+            InitializeComponent();
+        }
+
+        private static ProcessSharp getProcess()
+        {
+            var MCCProcess = System.Diagnostics.Process.GetProcessesByName("MCC-Win64-Shipping").FirstOrDefault();
+            if (MCCProcess == null)
+            {
+                MessageBox.Show("Could not locate process. Please ensure MCC is running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            var process = new ProcessSharp("MCC-Win64-Shipping", MemoryType.Remote);
+            return process;
+        }
+
+        public void setTickrate(int desired)
+        {
+            var process = getProcess();
+            process.Memory = new ExternalProcessMemory(process.Handle);
+
+            PatternScanner scanner = new PatternScanner(process["halo2.dll"]);
+            PatternScanResult result = scanner.Find(test);
+            if (!result.Found)
+            {
+                MessageBox.Show("Could not find tick rate in memory. Make sure you're in a game!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            byte[] offset = process.Memory.Read(result.ReadAddress + 3, 4);
+            IntPtr addressLocation = (result.ReadAddress + 7 + BitConverter.ToInt32(offset, 0));
+            byte[] addressValue = process.Memory.Read(addressLocation, 8);
+            IntPtr tickAddress = new IntPtr(BitConverter.ToInt64(addressValue, 0) + 2);
+            if (desired == 30)
+            {
+                process.Memory.Write(tickAddress, value30);
+            }
+
+            if (desired == 60)
+            {
+                process.Memory.Write(tickAddress, value60);
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                      .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void UIForm_Load(object sender, EventArgs e)
+        {
+            if (!IsAdministrator())
+            {
+                MessageBox.Show("MCCBounceEnable must be ran as administrator.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                setTickrate(30);
+            }
+            else
+            {
+                setTickrate(60);
+            }
+        }
+    }
+}
